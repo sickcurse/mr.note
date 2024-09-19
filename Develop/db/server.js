c366402a02271ef7b4ab86908065f8225c5b1657
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-09-19 14:24:42 -0400

    Added middleware for JSON parsing, URL-encoded data, and serving static files

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -4,3 +4,7 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
