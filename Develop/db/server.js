0162fb1bf4f404f207da5cb6dcf4f10264908938
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-09-19 14:21:28 -0400

    Set up Express server with file system and unique ID generation

---
+++ Develop/db/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+const PORT = process.PORT || 3001;
