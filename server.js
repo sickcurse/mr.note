4c3069ab7151047b01ff9e07f5e4c5dce1df7f1a
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-09-19 21:51:02 -0400

    Fixed POST route and corrected path for db.json

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Start the server
+
 app.listen(PORT, () => 
   console.log(`App listening at http://localhost:${PORT}`)
 );
 
-// HTML Routes
+
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
@@ -23,7 +23,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// API Routes
+
 app.get('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
@@ -32,40 +32,28 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-  const { title, text } = req.body;
-  if (title && text) {
-    const newNote = {
-      title,
-      text,
-      id: uuidv4(), // Generate unique ID
-    };
-
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      const notes = JSON.parse(data);
-      notes.push(newNote);
-
-      fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err) => {
+    const { title, text } = req.body;
+    if (title && text) {
+      const newNote = {
+        title,
+        text,
+        id: uuidv4(), 
+      };
+  
+      fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
-        res.json(newNote);
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+  
+        fs.writeFile('./Develop/db/db.json', JSON.stringify(notes, null, 2), (err) => {
+          if (err) throw err;
+          res.json(newNote);
+        });
       });
-    });
-  } else {
-    res.status(400).send('Note title and text are required');
-  }
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-  const noteId = req.params.id;
-
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    const notes = JSON.parse(data);
-    const newNotes = notes.filter((note) => note.id !== noteId);
-
-    fs.writeFile('./db/db.json', JSON.stringify(newNotes, null, 2), (err) => {
-      if (err) throw err;
-      res.json({ message: `Note ${noteId} deleted` });
-    });
+    } else {
+      res.status(400).json({ message: 'Note title and text are required' });
+    }
   });
-});
+  
+      
+    
